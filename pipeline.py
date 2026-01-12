1d42c97b7d27f1bcee65ad80ed4c5d6d2355b964
Alim Maknojia <alimmak04@gmail.com>
2026-01-12 10:34:42 -0600

    built the load csv function and loaded the csv

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,4 @@
-import requests
+import requests, csv
 
 def extract_recalls(make, model, year):
     url = f"https://api.nhtsa.gov/recalls/recallsByVehicle?make={make}&model={model}&modelYear={year}"
@@ -29,7 +29,14 @@ def transform_recalls(results):
         dictList.append(simplified)
     return dictList
 
-
+#usedictList here
+fieldnames = ["campaign_number", "make", "model", "year", "component", "summary"]
+# csv_path = "data/test.csv"
+def load_to_csv(data, filename):
+    with open(f"data/{filename}.csv", 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
 
 
 
@@ -46,6 +53,7 @@ def transform_recalls(results):
 brand = "Honda"
 carModel = "accord"
 productionYear = "2020"
-
+dataFile = "testData"
 data = extract_recalls(brand, carModel, productionYear)
-transform_recalls(data['results'])
+transformedData = transform_recalls(data['results'])
+load_to_csv(transformedData, dataFile)
