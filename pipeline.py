59d5ab5ebf4c553f11f888ac14d3761d9a4a8f76
Alim Maknojia <alimmak04@gmail.com>
2026-01-12 10:08:09 -0600

    built the function to clean up and transform the data a little bit

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,5 @@
 import requests
+
 def extract_recalls(make, model, year):
     url = f"https://api.nhtsa.gov/recalls/recallsByVehicle?make={make}&model={model}&modelYear={year}"
     response = requests.get(url)
@@ -11,8 +12,40 @@ def extract_recalls(make, model, year):
         print(f"Error: {response.status_code}")
         print(response.text)
 
+
+
+
+#use RecallData('results')
+def transform_recalls(results):
+    dictList = []
+    for recall in results:
+        simplified = {"campaign_number": recall["NHTSACampaignNumber"], 
+                      "make": recall["Make"].title(),
+                      "model": recall["Model"].title(),
+                      "year": recall["ModelYear"],
+                      "component": recall["Component"],
+                      "summary": recall["Summary"] 
+                      }
+        dictList.append(simplified)
+    print(dictList)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 brand = "Honda"
 carModel = "accord"
 productionYear = "2020"
 
-extract_recalls(brand, carModel, productionYear)
+data = extract_recalls(brand, carModel, productionYear)
+transform_recalls(data['results'])
