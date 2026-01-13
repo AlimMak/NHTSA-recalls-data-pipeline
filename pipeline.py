0fbda3c741208d7849691f49b8c70d9ab6de3774
Alim Maknojia <alimmak04@gmail.com>
2026-01-13 13:45:21 -0600

    fixed issues

--- pipeline.py
+++ pipeline.py
@@ -4,35 +4,35 @@ def extract_recalls(data):
 
     RecallData = []
     for car in data:
-        url = f"https://api.nhtsa.gov/recalls/recallsByVehicle?make={car.make}&model={car.model}&modelYear={car.year}"
+        url = f"https://api.nhtsa.gov/recalls/recallsByVehicle?make={car['make']}&model={car['model']}&modelYear={car['year']}"
         response = requests.get(url)
 
         if response.status_code == 200:
             RecallData.append(response.json())
-            print(f"Success! Found {RecallData['Count']} Recalls")
+            currCar = response.json()
+            print(f"Success! Found {currCar['Count']} Recalls")
         else:
             print(f"Error: {response.status_code}")
             print(response.text)
-            
+
     return RecallData
 
 
 
 #use RecallData('results')
-def transform_recalls(results):
+def transform_recalls(recallInfo):
     dictList = []
-    for recall in results:
-        simplified = {"campaign_number": recall["NHTSACampaignNumber"], 
-                      "make": recall["Make"].title(),
-                      "model": recall["Model"].title(),
-                      "year": recall["ModelYear"],
-                      "component": recall["Component"],
-                      "summary": recall["Summary"] 
-                      }
-        dictList.append(simplified)
+    for response in recallInfo:  # each API response
+        for recall in response['results']:  # each recall inside that response
+            simplified = {"campaign_number": recall["NHTSACampaignNumber"], 
+                          "make": recall["Make"].title(),
+                          "model": recall["Model"].title(),
+                          "year": recall["ModelYear"],
+                          "component": recall["Component"],
+                          "summary": recall["Summary"] 
+                          }
+            dictList.append(simplified)
     return dictList
-
-
 
 
 
@@ -61,9 +61,9 @@ dataFile = "testData"
 
 cars = [{"make": "Honda", "model": "Accord", "year": "2020"}, 
         {"make": "Ford","model": "F-150", "year": "2018"}, 
-        {"make": "BMW", "model": "M3", "year": "2018"}]
+        {"make": "Ford", "model": "Mustang", "year": "2018"}]
 
 
-data = extract_recalls(brand, carModel, productionYear)
-transformedData = transform_recalls(data['results'])
+data = extract_recalls(cars)
+transformedData = transform_recalls(data)
 load_to_csv(transformedData, dataFile)
